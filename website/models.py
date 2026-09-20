d9281d36008e5da76d21182e1867641a492f2a25
unknown <williamcarter808@gmail.com>
2026-09-20 10:56:35 +1000

    Switched to using integer ids since ids will be used for urls and uuids are ugly in urls

--- website/models.py
+++ website/models.py
@@ -4,7 +4,7 @@ from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
     __tablename__ = "Users"
-    id = db.Column(db.String(6), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.String(100), nullable=False)
     last_name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), index=True, nullable=False)
@@ -13,8 +13,8 @@ class User(db.Model, UserMixin):
 
 class Event(db.Model):
     __tablename__ = "Events"
-    id = db.Column(db.String(6), primary_key=True)
-    host_id = db.Column(db.String(6), db.ForeignKey('Users.id'), nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    host_id = db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=False)
     phone_number = db.Column(db.String(20))
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=False)
@@ -27,13 +27,13 @@ class Event(db.Model):
 
 class Offering(db.Model):
     __tablename__ = "Offerings"
-    id = db.Column(db.String(6), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(20), nullable=False)
 
 class EventOffering(db.Model):
     __tablename__ = "EventOfferings"
-    event_id = db.Column(db.String(6), db.ForeignKey('Events.id'), primary_key=True)
-    offering_id = db.Column(db.String(6), db.ForeignKey('Offerings.id'), primary_key=True)
+    event_id = db.Column(db.Integer, db.ForeignKey('Events.id'), primary_key=True)
+    offering_id = db.Column(db.Integer, db.ForeignKey('Offerings.id'), primary_key=True)
 
     event = db.relationship('Event', backref='offerings')
     offering = db.relationship('Offering', backref='events')
